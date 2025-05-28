bf1c521bde5c916e00999e9a726f9e0d8e5aba78
umerwe <uemyy1@gmail.com>
2025-05-28 08:33:30 +0500

    updated

--- src/api/baseQuery.js
+++ src/api/baseQuery.js
@@ -1,8 +1,13 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { logout, updateTokens } from '../store/slices/userSlice';
 
+const baseUrl =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_BASE_URL_PROD
+    : import.meta.env.VITE_API_BASE_URL_LOCAL;
+
 export const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_BASE_URL_LOCAL,
+  baseUrl,
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const { tokens } = getState().userSlice;
